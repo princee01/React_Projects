5aa4ba051035f791972232fb422ad0405582ff07
princee01 <upadhyayprince946@gmail.com>
2025-09-15 13:26:38 +0530

    build first page of project

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import Card_data from '../../assets/Cards/Cards_data'
 
 
-function TitleCrads() {
+
+function TitleCards() {
+
+const cardsRef = useRef();
+
+
+  useEffect(() => {
+    const handlewheel = (event) => {
+      event.preventDefault();
+      cardsRef.current.scrollLeft += event.deltaY;
+    }
+    
+    cardsRef.current.addEventListener('wheel', handlewheel)
+  }, [])
+  
   return (
-    <div className='titlecards'>
-      
+    <div className='title-cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {Card_data.map((card, index) => {
+          return <div className='card' key={index}>
+            <img src={card.image} alt="" />
+            <p>{ card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
-
-export default TitleCrads
+export default TitleCards

0256f6e8e618cf91ffdd234a5d09e41c6ee8c4b0
princee01 <upadhyayprince946@gmail.com>
2025-09-18 08:41:41 +0530

    routing done

--- 01_Netflix/src/main.jsx
+++ 01_Netflix/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Login from './pages/Login/Login.jsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/login',
+    element:<Login/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router } />
   </StrictMode>,
 )
--- 01_Netflix/src/pages/Login/Login.jsx
+++ 01_Netflix/src/pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import './Login.css'
 function Login() {
   return (
     <div className='login'>
-      
+       login page
     </div>
   )
 }
